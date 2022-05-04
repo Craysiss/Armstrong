fbc716d59c5f8f4bb50e21d7c0526b9ccff99b2a
Thiebaut Simon <s.thiebaut@etud.u-picardie.fr>
2022-05-04 10:31:29 +0200

    Création Prog Armstrong

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static bool pop(int nombre)
+        {
+            int centaine , dizaine , unite, reste;
+            
+            centaine = nombre / 100;
+            reste =  nombre % 100;
+            dizaine = reste / 10;
+            unite = reste % 10;  
+
+            if(centaine * centaine * centaine + dizaine * dizaine * dizaine + unite * unite * unite == nombre)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int nombre = 2;
+            Console.WriteLine(pop(nombre));
+        }
+    
+        
+    }
+
+
+}
