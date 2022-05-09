52ba759acaac35e09a932fe2198f7790bc7d3b83
Thiebaut Simon <s.thiebaut@etud.u-picardie.fr>
2022-05-09 15:27:51 +0200

    ajout doc

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace Armstrong
 {
     class Program
     {
+        //role : indique si le nombre donnee est un nombre de Armstrong
+        //parametre d'entre -> nombre a tester (en entre)
+        //valeur de sorie : true si valeur Armstrong
+        //                  faux si pas valeur Armstrong
         static bool pop(int nombre)
         {
             int centaine , dizaine , unite, reste;
